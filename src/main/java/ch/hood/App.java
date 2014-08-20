916e05e6b7d51e7f1c128f3e7b612921f869a90a
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2014-08-20 17:37:20 +0200

    pom.xml

---
+++ src/main/java/ch/hood/App.java
@@ -0,0 +1,13 @@
+package ch.hood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
