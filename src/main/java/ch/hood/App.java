b63227df4e9fee3e99ebd1095905495b696480c6
Stanislas Nanchen <stanislas.nanchen@gmail.com>
2014-08-23 15:40:30 +0200

    Add Query output.

--- src/main/java/ch/hood/App.java
+++ src/main/java/ch/hood/App.java
@@ -1,13 +1,16 @@
 package ch.hood;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import static ch.hood.jooq.schema1.Tables.A;
+import static ch.hood.jooq.schema2.Tables.B;
+import static org.jooq.impl.DSL.select;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(
+			select(A.ID, A.FLAG)
+				.from(A)
+				.join(B).on(B.NAME.eq(A.NAME))
+				.toString());
+	}
+
 }
